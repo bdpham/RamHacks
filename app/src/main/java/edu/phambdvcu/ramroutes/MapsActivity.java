19246f0a89275da32bbb95c68fd86e305a1f31ef
Rodrigo Moran <rigo.moran0@gmail.com>
2016-09-18 05:57:10 -0400

    Maps Api key

--- app/src/main/java/edu/phambdvcu/ramroutes/MapsActivity.java
+++ app/src/main/java/edu/phambdvcu/ramroutes/MapsActivity.java
@@ -1,18 +1,27 @@
 package edu.phambdvcu.ramroutes;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends FragmentActivity
+        implements OnMapReadyCallback {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private Marker mMarker;
@@ -21,13 +30,16 @@ public class MapsActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        setUpMapIfNeeded();
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+//        setUpMapIfNeeded();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        setUpMapIfNeeded();
+//        setUpMapIfNeeded();
     }
 
     /**
@@ -53,8 +65,10 @@ public class MapsActivity extends FragmentActivity {
                     .getMap();
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
+
                 setUpMap();
                 Log.d("DEBUG", "setting location");
+//                mMap.setMyLocationEnabled();
                 mMap.setMyLocationEnabled(true);
             }
         }
@@ -75,9 +89,48 @@ public class MapsActivity extends FragmentActivity {
         public void onMyLocationChange(Location location) {
             LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
             mMarker = mMap.addMarker(new MarkerOptions().position(loc));
-            if(mMap != null){
+            if (mMap != null) {
                 mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 16.0f));
             }
         }
     };
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+//        LocationManager locationManager = (LocationManager) getSystemService(
+//                Context.LOCATION_SERVICE);
+//        Criteria criteria = new Criteria();
+
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
+//        Location location = locationManager.getLastKnownLocation(
+//                locationManager.getBestProvider(criteria, false));
+
+//        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(
+//                location.getLatitude(), location.getLongitude()), 16));
+//
+//        CameraPosition cameraPosition = new CameraPosition.Builder()
+//                .target(new LatLng(location.getLatitude(), location.getLongitude()))
+//                .zoom(17)
+//                .bearing(90)
+//                .tilt(40)
+//                .build();
+//        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+        LatLng rva = new LatLng(37.5,-77.4);
+        mMap.addMarker(new MarkerOptions().position(rva).title("Marker in RVA"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(rva,16));
+        mMap.setMyLocationEnabled(true);
+//        mMap.
+    }
 }
